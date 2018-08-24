43a63e2c04e27261e6f6f7d0313b7e438b732a22
Wie Kiang H <me@wiekiang.com>
2018-08-24 18:25:30 +0700

    Prediction breed using Resnet50

--- detect.py
+++ detect.py
@@ -1,11 +1,18 @@
+### TODO: Write your algorithm.
+### Feel free to use as many code cells as needed.
 import random
 import cv2                
 import matplotlib.pyplot as plt
 import numpy as np
 
-from sklearn.datasets import load_files       
-from keras.utils import np_utils
+from sklearn.datasets import load_files
 from glob import glob
+
+from keras.applications.resnet50 import ResNet50
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.preprocessing import image  
+from keras.utils import np_utils                
+from tqdm import tqdm
 
 random.seed(8675309)
 
@@ -32,7 +39,8 @@ dog_names = [item[20:-1] for item in sorted(glob("dogImages/train/*/"))]
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
 
 # load color (BGR) image
-img = cv2.imread('images/Baby-6.jpg')
+image_file = 'images/prow-featured.jpg'
+img = cv2.imread(image_file)
 # convert BGR image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -50,3 +58,52 @@ cv_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 # display the image, along with bounding box
 plt.imshow(cv_rgb)
 plt.show()
+
+
+
+# define ResNet50 model
+ResNet50_model = ResNet50(weights='imagenet')
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
+
+def ResNet50_predict_labels(img_path):
+    # returns prediction vector for image located at img_path
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
+
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return ((prediction <= 268) & (prediction >= 151))  
+
+from extract_bottleneck_features import *
+
+def Resnet50_predict_breed(img_path):
+    # extract bottleneck features
+    bottleneck_feature = extract_Resnet50(path_to_tensor(img_path))
+    # obtain predicted vector
+    predicted_vector = Resnet50_model.predict(bottleneck_feature)
+    # return dog breed that is predicted by the model
+    return dog_names[np.argmax(predicted_vector)]
+
+if face_detector(image_file):
+    print("It's look like", Resnet50_predict_breed(image_file))
+elif dog_detector(image_file):
+    print("It's look like", Resnet50_predict_breed(image_file))
+else:
+    print("Unknown object") 
